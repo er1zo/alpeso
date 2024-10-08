4ca9050223112a4b9b060d59144d0bc3c50de50e
er1zo <aleksei@intelligere.ee>
2024-10-08 11:32:54 +0300

    adds another header

--- webapp/index.php
+++ webapp/index.php
@@ -1,20 +1,18 @@
 <?php
-// Check if the request is a POST request
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Get all the headers from the request
     $headers = getallheaders();
 
-    // Check if the 'NotBad' header exists and its value is 'true'
     if (isset($headers['NotBad']) && $headers['NotBad'] === 'true') {
-        // Respond with "ReallyNotBad"
         echo "ReallyNotBad";
-    } else {
-        // Respond with an error message if the header is not set or incorrect
+    } 
+    elseif (isset($headers['World']) && $headers['World'] === 'true') {
+        echo "Hello World";
+    }
+    else {
         http_response_code(400);
         echo "Bad Request: 'NotBad' header is missing or incorrect.";
     }
 } else {
-    // Respond with an error message if the request method is not POST
     http_response_code(405);
     echo "Method Not Allowed: Only POST requests are supported.";
 }
